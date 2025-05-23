6bea796d3b022de79331d70f1f3fb09431b94201
coko9437 <coko9437@gmail.com>
2025-05-23 17:38:45 +0900

    25_05_23_오후)테이블생성등

--- 25_05_23.sql
+++ 25_05_23.sql
@@ -5,61 +5,280 @@
 
 -- 예시
 
-UPDATE EMP SET SAL = 1000;
+update emp
+   set
+   sal = 1000;
 
 -- 복사한 테이블에서 연습하기.
 -- EMP -> EMP_COPY 테이블 복사
-CREATE TABLE EMP_COPY AS SELECT * FROM EMP;
+create table emp_copy
+   as
+      select *
+        from emp;
 
-SELECT * FROM EMP_COPY;
+select *
+  from emp_copy;
 
 -- 전체 수정
-UPDATE EMP_COPY SET SAL = 1000;
-COMMIT;  --반영
+update emp_copy
+   set
+   sal = 1000;
+commit;  --반영
 
 -- 조건 수정
-UPDATE EMP_COPY SET SAL = 2000 WHERE DEPTNO = 10;
-ROLLBACK; --되돌리기
+update emp_copy
+   set
+   sal = 2000
+ where deptno = 10;
+rollback; --되돌리기
 
 -- 서브쿼리 활용
-UPDATE EMP_COPY SET SAL = 2000 
-    WHERE DEPTNO = (SELECT DEPTNO FROM EMP WHERE JOB = 'ALLEN');
+update emp_copy
+   set
+   sal = 2000
+ where deptno = (
+   select deptno
+     from emp
+    where job = 'ALLEN'
+);
 
 -- 퀴즈1) DEPT -> DEPT_TEMP2 테이블 복사해보기
 --  DEPT_TEMP2 테이블에서 부서번호가 20번인 행의 지역을 'JEJU'로 수정
-CREATE TABLE DEPT_TEMP2 AS SELECT * FROM DEPT;
-SELECT * FROM DEPT_TEMP2;
-UPDATE DEPT_TEMP2 SET LOC = 'JEJU' WHERE DEPTNO=20;
+create table dept_temp2
+   as
+      select *
+        from dept;
+select *
+  from dept_temp2;
+update dept_temp2
+   set
+   loc = 'JEJU'
+ where deptno = 20;
 -- 퀴즈2) DEPT_TEMP2 테이블의 전체지역을 'SEOUL'로 수정
-UPDATE DEPT_TEMP2 SET LOC = 'SEOUL';
+update dept_temp2
+   set
+   loc = 'SEOUL';
 -- 퀴즈3) 직책이 'MANAGER'인 사원의 급여를 5000으로 수정
 -- UPDATE DEPT_TEMP2 SET SAL = 5000 WHERE JOB = 'MANAGER';
 
-UPDATE EMP_COPY SET SAL = 5000 WHERE JOB = 'MANAGER';
+update emp_copy
+   set
+   sal = 5000
+ where job = 'MANAGER';
 
 -- 테이블에 있는 데이터 삭제하기
 
 -- 조건에 맞는 데이터 삭제 (EMP테이블에서 DEPTNO=30인 행을 삭제하기.)
-DELETE FROM EMP_COPY WHERE DEPTNO=30;
-SELECT * FROM EMP_COPY;
+delete from emp_copy
+ where deptno = 30;
+select *
+  from emp_copy;
 -- DELETE FROM 테이블명 WHERE 조건;
 -- <서브쿼리> 활용한 삭제
-DELETE FROM emp_copy WHERE DEPTNO
-    IN ( SELECT DEPTNO FROM DEPT WHERE LOC = 'DALLAS');
+delete from emp_copy
+ where deptno in (
+   select deptno
+     from dept
+    where loc = 'DALLAS'
+);
 
 -- 테이블에 있는 모든 데이터 삭제
-DELETE FROM EMP_COPY;
+delete from emp_copy;
 
 --서브쿼리로 'DALLAS'에 있는 부서의 사원 삭제
-DELETE FROM EMP_COPY WHERE DEPTNO
-    IN ( SELECT DEPTNO FROM DEPT WHERE LOC = 'DALLAS');
-SELECT DEPTNO FROM DEPT WHERE LOC = 'DALLAS';
+delete from emp_copy
+ where deptno in (
+   select deptno
+     from dept
+    where loc = 'DALLAS'
+);
+select deptno
+  from dept
+ where loc = 'DALLAS';
 
 -- 퀴즈1) EMP_TEMP2 테이블에서 급여가 3000 이상인 사원 삭제
-CREATE TABLE EMP_TEMP AS SELECT * FROM EMP;
-SELECT * FROM EMP;
-DELETE FROM EMP_TEMP2 WHERE SAL >=3000;
+create table emp_temp
+   as
+      select *
+        from emp;
+select *
+  from emp;
+delete from emp_temp2
+ where sal >= 3000;
 -- 퀴즈2)EMP_TEMP2 테이블에서 부서번호가 10 또는 20인 사원을 삭제.
-DELETE FROM EMP_TEMP2 WHERE DEPTNO IN(10,20);
+delete from emp_temp2
+ where deptno in ( 10,
+                   20 );
 -- 퀴즈3) EMP_TEMP2 테이블의 모든 데이터를 삭제.
-DELETE FROM EMP_TEMP2;
+delete from emp_temp2;
+
+
+-- ////////////////////////////////////////////////
+
+-- 하나의 단위로 데이터를 처리하는 트랜잭션
+
+select *
+  from emp_copy;
+
+insert into emp_copy
+   select *
+     from emp;
+commit; -- 반영
+
+-- 순서1) EMP_COPY 테이블내 데이터 변경
+update emp_copy
+   set
+   sal = sal + 10000
+ where deptno = 10;
+    
+-- 순서2) 트랜잭션 저장
+commit;
+-- 순서3) 트랜잭션 취소
+rollback;
+-- 순서4) 특정 지점으로 설정
+savepoint sp1;
+-- 순서5) 특정 지점으로 롤백
+rollback to sp1;
+
+-- 퀴즈1) EMP -> EMP_COPY2 테이블 복사, 
+    -- 부서 번호가 20인 사원들의 급여를 10% 인상 후, 조건에 따라 되돌릴수 있도록 SAVEPOINT (SP2)를 설정하시오.
+savepoint sp2;
+create table emp_copy2
+   as
+      select *
+        from emp;
+select *
+  from emp_copy2;
+update emp_copy2
+   set
+   sal = sal * 1.1
+ where deptno = 20;
+-- 퀴즈2)사번이 7839인 사원의 급여를 5000으로 변경, ROLLBACK만 하기
+commit;
+    --SAVEPOINT SP2;
+update emp_copy2
+   set
+   sal = 6000
+ where empno = 7839;
+rollback;
+-- 퀴즈3)여러 UPDATE후 COMMIT하지않고 전체를 ROLLBACK하기
+update emp_copy2
+   set
+   sal = sal * 0.5
+ where deptno in ( 10,
+                   20 );
+update emp_copy2
+   set
+   mgr = 2025
+ where deptno in ( 30,
+                   50 );
+update emp_copy2
+   set
+   mgr = 2025
+ where job = 'MANAGER';
+select *
+  from emp_copy2;
+rollback;
+
+
+-- 객체를 생성, 변경, 삭제하는 데이터 정의어 : DDL
+
+-- CREATE : 객체를 생성하는 명령어
+-- ALTER : 객체를 변경하는 명령어
+-- DROP : 객체를 삭제하는 명령어
+-- TRUNCATE : 테이블의 모든 데이터를 삭제하는 명령어
+
+-- 테이블 이름 명명 할 때, 각각 언어의 예약어를 사용하면 안됨.
+create table member (
+   member_id    number(5) primary key, -- PK = 기본키.. NOT NULL + UNIQUE(중복불가)
+   member_name  varchar2(20) not null, -- 값이 비어있으면 안됨.
+   member_email varchar2(50) not null
+);
+select *
+  from member;
+-- 샘플 데이터 추가
+insert into member_info (
+   mamber_id,
+   member_name,
+   member_email
+) values ( 1,
+           '홍길동',
+           'HONG@GMAIL.COM' );
+
+insert into member_info (
+   mamber_id,
+   member_name,
+   member_email
+) values ( 2,
+           '청길동',
+           'CHEONG@GMAIL.COM' );
+
+insert into member_info (
+   mamber_id,
+   member_name,
+   member_email
+) values ( 3,
+           '흑길동',
+           'BLACK@GMAIL.COM' );
+
+-- 테이블 구조 변경
+alter table member add member_phone varchar2(20);
+
+-- 테이블 이름 변경하기 MEMBER -> MEMBER_INFO
+alter table member rename to member_info;
+select *
+  from member_info;
+
+-- 테이블 내용만 전체 삭제하기 -> 빈 테이블만 남음.
+truncate table member_info;
+-- 테이블 자체를 삭제하기
+drop table member_info;
+
+-- 퀴즈1) 테이블 : BOARD // 컬럼 : BOARD_ID(NUMBER 5), TITLE(VARCHAR2(30)), CONTENT(VARCHAR2(300)), WRITER(VARCHAR2(30)), REGDATE(DATE)
+create table board (
+   board_id number(5) primary key,
+   title    varchar2(30) not null,
+   content  varchar2(300) not null,
+   writer   varchar2(30) not null,
+   regdate  date not null
+);
+select *
+  from board;
+insert into board (
+   board_id,
+   title,
+   content,
+   writer,
+   regdate
+) values ( 1,
+           '제목1',
+           '내용1',
+           '작성자1',
+           sysdate );
+insert into board (
+   board_id,
+   title,
+   content,
+   writer,
+   regdate
+) values ( 2,
+           'CREATE11',
+           'CONTENT11',
+           'WRITTER11',
+           sysdate );
+insert into board (
+   board_id,
+   title,
+   content,
+   writer,
+   regdate
+) values ( 3,
+           'CREATE22',
+           'CONTENT22',
+           'WRITTER22',
+           sysdate );
+-- 퀴즈2) BOARD 테이블에 특정 컬럼의 타입 변경해보기. // WRITER VARCHART2 40으로 변경
+alter table board modify
+   writer varchar2(40);
+-- 퀴즈3) BOARD 테이블에 내용만, 삭제해보기
+truncate table board;
--- NOSQL-TEST.mongodb.js
+++ NOSQL-TEST.mongodb.js
@@ -18,7 +18,7 @@ db.users.insertOne({
     name: '홍길동',
     age: 20,
     favorite: ['apple', 'banana'],
-    })
+})
 
 
 //조회
@@ -28,33 +28,58 @@ db.users.find();
 //수정
 //조건에 맞는 첫번째 데이터 수정
 //db.users.updateOne({조건}, {$set :{수정할값} }) //조건에 맞는 첫번째 데이터 수정
-db.users.updateOne({name: '홍길동'},  //조건
-    {$set: {age: 30 } })//수정할 값>
+db.users.updateOne({ name: '홍길동' },  //조건
+    { $set: { age: 30 } })//수정할 값>
 
 //삭제
 //조건에 맞는 첫번째 데이터 삭제
 //db.users.deleteOne({조건})//조건에 맞는 첫번째 데이터 삭제
-db.users.deleteOne({name: '홍길동'})//이름이 홍길동인 사람을 삭제함
+db.users.deleteOne({ name: '홍길동' })//이름이 홍길동인 사람을 삭제함
 
 // capped collection : 컬렉션 = '테이블'
 // 컬렉션의 용량이 초과하게되면, 오래된 데이터부터 차례대로 삭제하는기능
 // db.createCollection('컬렉션명', {capped: true, size: [용량]})
-db.createCollection('logs' ,{capped: true, size: 5000})//5KB 인 용량의 컬렉션을 생성, 
+db.createCollection('logs', { capped: true, size: 5000 })//5KB 인 용량의 컬렉션을 생성, 
 // 부가기능으로 용량 초과시 오래된 데이터 삭제
 // 샘플데이터 추가, 반복문을 이용해서 샘플로 1000개 추가
-for (let i = 2000; i < 3000; i++) {
+for (let i = 0; i < 10; i++) {
     db.logs.insertOne({
-        message : `로그메시지 ${i}`,
-        timestamp : new Date(), //현재날짜 / 오라클로 표현하면 sysdate()
+        message: `로그메시지 ${i}`,
+        timestamp: new Date(), //현재날짜 / 오라클로 표현하면 sysdate()
     })
 }
 db.logs.find()
 
-db.createCollection('logs2' ,{capped: true, size: 5000})//5KB 인 용량의 컬렉션을 생성, 
+db.createCollection('logs2', { capped: true, size: 5000 })//5KB 인 용량의 컬렉션을 생성, 
 
 for (let i = 2000; i < 3000; i++) {
     db.logs2.insertOne({
-        message : `로그메시지 ${i}`,
-        timestamp : new Date(), //현재날짜 / 오라클로 표현하면 sysdate()
+        message: `로그메시지 ${i}`,
+        timestamp: new Date(), //현재날짜 / 오라클로 표현하면 sysdate()
     })
 }
+// 퀴즈1) 한개 문서 삽입, 컬렉션 명 : users2
+// 이름, 생년월일, 좋아하는 음식, 등록날짜, 성별 등
+db.createCollection('users2');//
+db.users2.insertOne({
+    name: '추교문', //이름, 문자열로 저장
+    birth: '1994-03-23',//생년월일, 문자열로 저장
+    favorite: ['서브웨이', '딸바쥬스'],//좋아하는 음식 배열로 저장
+    regdate: new Date(),//등록날짜, 현재날짜로 저장  
+    gender: '남자'//성별, 문자열로 저장
+})
+db.users2.find()
+// 퀴즈2) 컬렉션 명 : users2 , 수정해보기
+// 항목들 중에 수정2 문자열 추가해보기
+db.users2.updateOne(
+    { favorite: ['서브웨이', '딸바쥬스'] },  //조건
+    { $set: { favorite: ['맥도날드', '수박쥬스'] } })//수정할 값>
+// 퀴즈3) users2 컬렉션에서 등록한 항목 삭제해보기.
+
+db.users2.deleteOne(
+    {
+        name: '추교문',
+        favorite: ['맥도날드', '수박쥬스']
+    }
+)
+
